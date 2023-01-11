74ddd036453496060fda7fbc5e69b49e7f9be4ac
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-11 13:14:18 -0800

    Challenge 3

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const deckBtn = selectComponent("deck-btn");
+const newDeckBtn = selectComponent("deck-btn");
 
 function selectComponent(elementId) {
   let component = document.getElementById(elementId);
   return component;
 }
 
-deckBtn.addEventListener("click", fetchDeck);
+newDeckBtn.addEventListener("click", fetchDeck);
 
 function fetchDeck() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
+
+
+setTimeout(() => {
+    console.log("I finally ran!")
+}, 2000)
