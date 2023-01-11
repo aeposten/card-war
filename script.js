9fd83633a38d7c92c149a4cd6ceb7475d612901c
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-11 14:02:43 -0800

    Challenge 7

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-const newDeckBtn = selectComponent("deck-btn");
+// const newDeckBtn = selectComponent("deck-btn");
 
-function selectComponent(elementId) {
-  let component = document.getElementById(elementId);
-  return component;
-}
-
-newDeckBtn.addEventListener("click", fetchDeck);
-
-function fetchDeck() {
-  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
-
-
-// function callback() {
-//     console.log("I finally ran!")
+// function selectComponent(elementId) {
+//   let component = document.getElementById(elementId);
+//   return component;
 // }
 
-// setTimeout(callback, 2000)
+// newDeckBtn.addEventListener("click", fetchDeck);
+
+// function fetchDeck() {
+//   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
 
 
-const people = [
-    { name: "Jack", hasPet: true, age: 12 },
-    { name: "Jill", hasPet: false, age: 18 },
-    { name: "Alice", hasPet: true, age: 22 },
-    { name: "Bob", hasPet: false, age: 32 },
-]
-
-function petOwners(person) {
-   return person.hasPet
-}
-
-function getAdults(person) {
-    return person.age >= 18;
-}
-
-const petPeople = people.filter(petOwners)
-const adults = people.filter(getAdults)
-
+document.getElementById("deck-btn").addEventListener("click", function() {
+    console.log("click")
+})
