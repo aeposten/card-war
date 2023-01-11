b03b96c0640d477cdc0da576a8f28a8475058428
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-11 15:15:35 -0800

    Can draw and render cards on page

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const newDeckBtn = selectComponent("deck-btn");
-const drawButton = selectComponent("draw-card-btn")
-let deckId = "";
+const drawButton = selectComponent("draw-card-btn");
+const cardsEl = selectComponent("player-cards");
+
+let deckId;
+let playerCards;
 
 function selectComponent(elementId) {
   let component = document.getElementById(elementId);
@@ -10,18 +13,37 @@ function selectComponent(elementId) {
 newDeckBtn.addEventListener("click", fetchDeck);
 drawButton.addEventListener("click", drawCards);
 
-
 function fetchDeck() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then((response) => response.json())
     .then((data) => {
       deckId = data.deck_id;
-      console.log(deckId)
+      localStorage.setItem("deckId", deckId);
     });
 }
 
 function drawCards() {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-        .then((response) => response.json())
-        .then((data) => console.log(data.cards))
+  fetch(
+    `https://deckofcardsapi.com/api/deck/${localStorage.getItem(
+      "deckId"
+    )}/draw/?count=2`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      playerCards = data.cards;
+      renderCards(playerCards);
+    });
+}
+
+function renderCards(cardsArray) {
+  cardsEl.innerHTML = cardsArray
+    .map(
+      ({ image, value, suit }, index) => `
+        <div class="player-${index + 1}">
+        <h3>Player ${index + 1}</h3>
+        <img class="card-img" src=${image}  alt="${value} of ${suit}"/>
+        </div>
+    `
+    )
+    .join("");
 }
